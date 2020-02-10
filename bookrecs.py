7ae37651b8a36d247c71e32a3dba0a59c3874d7f
brownrnoah <brownrnoah@gmail.com>
2020-02-09 19:11:23 -0700

    Finished up what was needed for this project.

--- bookrecs.py
+++ bookrecs.py
@@ -1,9 +1,19 @@
+"""
+Noah Brown
+CS 1410-602
+Febuary 9th, 2020
+Book Recommendation Project
+"""
+
+#Reading in the list of books from the file and making a list of them. 
 readBooks = open("booklist.txt", "r")
 bookList = []
 for book in readBooks:
     tempList = book.strip().split(",")
     bookList.append((tempList[0],tempList[1]))
 
+#Reading in the list of readers and their ratings given for each book that they read.
+#Putting them in dictionaries nested in a list, giving them keys for their name and ratings.
 readRatings = open("ratings.txt","r")
 count = 1
 name = "name"
@@ -21,6 +31,8 @@ for line in readRatings:
         ratingsList.append(newDic)
     count += 1
 
+#Looping over the list of readers and their ratings to assign them their top two closest "friends".
+#I realize that in the next project this will need to be changed because more than two friends will be selected. 
 for name in ratingsList:
     friendList = []
     for other in ratingsList:
@@ -44,11 +56,19 @@ for name in ratingsList:
             largeName = friend["friend"]        
     name["friends"] = [largestName,largeName]
 
+#Function that returns the top two friends given a name as a parameter. 
 def friends(name):
     for x in ratingsList:
         if name == x["name"]:
-            print(x["friends"])
+            return sorted(x["friends"])
+        
+#I know I was supposed to add this function for checking scores of users to find their friends, and I know the next project will ask for it.
+#I got away without having to implement it for this one, but I will work on adding it for the next one. But for now I didn't want to break apart all
+#that I have already programmed for lack of line. I will deal with that headache on the next one.
+def dotprod(x,y):
+    pass
 
+#Assigning new keys to each user's dictionary for their unread books and their favorite books which they scored 3 or 5 on.
 for user in ratingsList:
     goodBooks = []
     unreadBooks = []
@@ -61,7 +81,9 @@ for user in ratingsList:
         count += 1    
     user["goodbooks"] = goodBooks
     user["unreadbooks"] = unreadBooks
-            
+
+#Function that returns recommended books for a given user based off of the books they haven't read and the books that their friends have read.
+#Added sorting by Author last name using a Lambda function.
 def recommend(name):
     tempList = []
     theirRecommended = []
@@ -77,18 +99,28 @@ def recommend(name):
             for book in tempList:
                 if book in user["unreadbooks"]:
                     theirRecommended.append(book)
-    
+    sortedList = []
     theirRecommended = list(dict.fromkeys(theirRecommended))
-    print(sorted(theirRecommended, key=lambda x: x[0].split(" ")[-1]))
-            
-    
-
+    sortedList.extend(sorted(theirRecommended, key=lambda x: x[0].split(" ")[-1]))
+    return sortedList
+        
+#Function that returns everyone's recommended books and their name and friends names. Sorted by Reader's first name.
+def report():
+    myString = ""
+    for person in sorted(ratingsList, key = lambda i: i['name']):
+        myString += (person['name'] + " : " + str(sorted(person["friends"])))
+        myString += "\n"
+        for y in recommend(person['name']):
+            myString += "\t" + str(y) + "\n"
+        myString += "\n"
+    return myString
+        
+#Main function where in the report function will be called and saved to a file.      
 def main():
-    #friends("ben")
-    recommend("albus dumbledore")
- #with open('recommendations.txt', 'w') as rec_file:
- #print(report(), file=rec_file)
- 
+    with open('recommendations.txt', 'w') as rec_file:
+#        For some reason I couldn't get this line to both print and write it would only write, so I added another print.
+        print(report(), file=rec_file)
+        print(report())
  
 if __name__ == '__main__':
  main()
